5eb59a2a49dcc589a7f38a163bc68ef0ec1cd83f
Hunter Muench <hdmuench@cox.net>
2019-09-17 10:02:26 -0400

    Edited when input = 1

--- reportCard.cpp
+++ reportCard.cpp
@@ -3,20 +3,27 @@
 using namespace std;
 
 class card {
-    int grade;
+    private static int num = 0;
+    int grade, id;
     string name;
     list<string> classes;
-    
+public:
     void card() {
         grade = 9;
         classes = {"Geometry", "World History", "Spanish", "English", "Biology"};
         name = "Student";
+        id = num;
+        num++;
     }
     void card(int g, string n, list<string> c) {
         grade = g;
         name = n;
         classes = c;
+        id = num;
+        num++;
     }
+    void changeName (string n) {name = n;}
+    void chnageGrade (int g) {grade = g;}
     
 };
 
@@ -24,12 +31,27 @@ class card {
 
 int main () {
     int input;
-    string name;
     cout << "Welcome to the report card interface!\nSelect 1 to add a student, 2 to view a classes average grade, 3 to view your grades, and 4 to exit.\n";
     cin >> input;
     if (input == 1) {
-        cout << "Please enter the full name of the student";
+        string name;
+        int grade;
+        list<string> classes;
+        cout << "Please enter the full name of the student: ";
         getline(cin,name);
+        cout << "Enter the grade number: ";
+        cin >> grade;
+        cout << "How many classes do you want to enter?";
+        int c;
+        cin >> c;
+        string title;
+        for(int i=1; i<=c; i++) {
+            cout << "Enter name for class #" << i << ": ";
+            getline(cin, title);
+            classes.push_back(title);
+        }
+        card(grade, name, classes);
+        cout << "You have successfully created " << name;
     } else if (input == 2) {
         cout << "Please select a class from the list:\n";
         cout << "Biology (1) Chemistry (2) Physics (3) Calculus (4) Statistics (5)";
